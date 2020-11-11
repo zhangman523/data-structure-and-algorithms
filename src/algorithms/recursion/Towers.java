09be02c939d00ce9a8d61fe390b47a18f6443dd8
zm <zm@cheok.com>
2020-11-11 17:12:12 +0800

    add Towers

---
+++ src/algorithms/recursion/Towers.java
@@ -0,0 +1,22 @@
+package algorithms.recursion;
+
+/**
+ * Java 递归实现的汉诺塔问题
+ */
+public class Towers {
+    static int nDisks = 3;
+
+    public static void main(String[] args) {
+        doTowers(nDisks, 'A', 'B', 'C');
+    }
+
+    public static void doTowers(int topN, char from, char inter, char to) {
+        if (topN == 1) {
+            System.out.println("Disk 1 from " + from + " to " + to);
+        } else {
+            doTowers(topN - 1, from, to, inter); // from -->inter
+            System.out.println("Disk " + topN + " from " + from + " to " + to);
+            doTowers(topN - 1, inter, from, to);
+        }
+    }
+}
